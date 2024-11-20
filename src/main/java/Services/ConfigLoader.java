fb77ae9f4864299ec908da25386a733ce97139b7
Forvi <nforvi@mail.ru>
2024-11-20 18:00:34 +0500

    работа с api

---
+++ src/main/java/Services/ConfigLoader.java
@@ -0,0 +1,17 @@
+package Services;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigLoader {
+    public static String getApiKey(String key) {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+            return properties.getProperty(key);
+        } catch (IOException e) {
+            throw new RuntimeException("Ключа не существует", e);
+        }
+    }
+}
