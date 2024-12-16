77769c455767378d08a8234c085c357fde6a5170
Forvi <nforvi@mail.ru>
2024-12-16 20:35:14 +0500

    parser update

--- src/main/java/Entities/StudentEntity.java
+++ src/main/java/Entities/StudentEntity.java
@@ -1,3 +1,48 @@
 package Entities;
 
-public record StudentEntity(String name, String group) { }
+import java.util.ArrayList;
+
+public class StudentEntity {
+    private final String name;
+    private final String group;
+    private final int pointsCount;
+    private final ArrayList<TopicEntity> topicsForStudent;
+
+    public StudentEntity(String name, String group, int pointsCount, ArrayList<TopicEntity> topicsForStudent) {
+        this.name = name;
+        this.group = group;
+        this.pointsCount = pointsCount;
+        this.topicsForStudent = topicsForStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getPointsCount() {
+        return pointsCount;
+    }
+
+    public ArrayList<TopicEntity> getTasks() {
+        return topicsForStudent;
+    }
+
+    @Override
+    public String toString() {
+        var topicsBuilder = new StringBuilder();
+        for (var topic : topicsForStudent) {
+            topicsBuilder.append(topic.toString()).append("\n");
+        }
+
+        return String.format("""
+                NAME: %s
+                GROUP: %s
+                POINTS: %d
+                MODULES FOR STUDENT: %s
+                """, name, group, pointsCount, topicsBuilder.toString().trim());
+    }
+}
